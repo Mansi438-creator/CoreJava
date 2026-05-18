42eb6dcebaf79a3c05a2d860fc2c3d40de7707e2
Mansi <mansinannaware622@gmail.com>
2026-05-18 17:44:29 +0530

    Add nested if-else logic to find greatest among three numbers

---
+++ src/conditionalstatement/ConditionalStatements.java
@@ -0,0 +1,31 @@
+package conditionalstatement;
+
+public class ConditionalStatements {
+
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 3;
+        int c = 7;
+
+        if (a >= b) {
+
+            if (a >= c) {
+                System.out.println("Greatest number is: " + a);
+            } 
+            else {
+                System.out.println("Greatest number is: " + c);
+            }
+
+        } 
+        else {
+
+            if (b >= c) {
+                System.out.println("Greatest number is: " + b);
+            } 
+            else {
+                System.out.println("Greatest number is: " + c);
+            }
+        }
+    }
+}
