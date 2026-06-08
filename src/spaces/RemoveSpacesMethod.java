565cce4f45f12da62d453727a2082d7a3386b852
Mansi <mansinannaware622@gmail.com>
2026-06-08 22:43:38 +0530

    add program to remove spaces from string using replace() method

---
+++ src/spaces/RemoveSpacesMethod.java
@@ -0,0 +1,18 @@
+package spaces;
+
+public class RemoveSpacesMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        String str = "Ja va Pro gram";
+
+		        str = str.replace(" ", "");
+
+		        System.out.println(str);
+		    }
+		
+
+	}
+
+
