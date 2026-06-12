c7cfc215b9d2a51900435a52a9bee9774ccd4924
Mansi <mansinannaware622@gmail.com>
2026-06-12 18:48:35 +0530

    Remove special characters and keep only letters and digits.

---
+++ src/characters/RemoveSpecialCharacters.java
@@ -0,0 +1,31 @@
+package characters;
+
+public class RemoveSpecialCharacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        String str = "Java@123#";
+		        String result = "";
+
+		        // Traverse each character of the string
+		        for (int i = 0; i < str.length(); i++) {
+
+		            // Get the current character
+		            char ch = str.charAt(i);
+
+		            // Check if the character is a letter or digit
+		            if (Character.isLetterOrDigit(ch)) {
+		                // Add it to the result string
+		                result += ch;
+		            }
+		        }
+
+		        // Print the string after removing special characters
+		        System.out.println(result);
+		    }
+		
+
+	}
+
+
