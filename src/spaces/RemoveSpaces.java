9560301b2d6e64a0c8a28161731d583767ca63c3
Mansi <mansinannaware622@gmail.com>
2026-06-15 22:37:26 +0530

    Remove spaces from a string

---
+++ src/spaces/RemoveSpaces.java
@@ -0,0 +1,27 @@
+package spaces;
+
+import java.util.Scanner;
+public class RemoveSpaces {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		        Scanner sc = new Scanner(System.in);
+		        System.out.print("Enter String: ");
+		        String str = sc.nextLine();
+
+		        String result = "";
+
+		        for (int i = 0; i < str.length(); i++) {
+		            if (str.charAt(i) != ' ') {
+		                result += str.charAt(i);
+		            }
+		        }
+
+		        System.out.println("Result = " + result);
+		    }
+		
+	}
+
+
