f4855596137866959c205b4cbbc61d3131dbdbab
Mansi <mansinannaware622@gmail.com>
2026-06-22 00:06:27 +0530

    Add program to calculate factorial of a number

---
+++ src/com/java/core/numbers/Factorial.java
@@ -0,0 +1,27 @@
+package com.java.core.numbers;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number to find its factorial: ");
+        int num = sc.nextInt();
+        
+        long factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+        
+        System.out.println("The factorial of " + num + " is: " + factorial);
+        
+        sc.close();
+    }
+
+
+	}
+
+
