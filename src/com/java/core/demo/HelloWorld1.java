d832a8884f5c84daf9d60fdc8ec9ddcf9c1ed2fb
Mansi <mansinannaware622@gmail.com>
2026-06-23 00:13:32 +0530

    Add a new hello program

---
+++ src/com/java/core/demo/HelloWorld1.java
@@ -0,0 +1,10 @@
+package com.java.core.demo;
+
+public class HelloWorld1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello! Welcome to Java programming.");
+	}
+
+}
