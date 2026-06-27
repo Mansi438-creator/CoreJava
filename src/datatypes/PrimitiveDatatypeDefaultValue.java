314d2d47e8662d08ce63dc6c2e2eb7ae78aa9580
Mansi <mansinannaware622@gmail.com>
2026-06-27 23:50:07 +0530

    Finding default values of primitive datatypes

---
+++ src/datatypes/PrimitiveDatatypeDefaultValue.java
@@ -0,0 +1,33 @@
+package datatypes;
+
+public class PrimitiveDatatypeDefaultValue{
+
+static int a;
+static short s;
+static long l;
+static byte b;
+static float f;
+static double d;
+static char c;
+static boolean bo;
+static String str;
+
+public static void main(String[] args) {
+System.out.println("default value of int is : " +a);
+
+System.out.println("default value of short is : " +s);
+System.out.println("default value of long is : " +l);
+
+System.out.println("default value of byte is : " +b);
+
+System.out.println("default value of float is : " +f);
+
+System.out.println("default value of double is : " +d);
+
+System.out.println("default value of char is : " +c);
+
+System.out.println("default value of boolean is : " +bo);
+
+System.out.println("default value of String is : " +str);
+ }
+}
